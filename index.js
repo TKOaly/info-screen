3b6303a56d3397bb11450a0b1d444a6a757b05db
Jani Luukko <jj.luukko@gmail.com>
2019-02-18 15:10:57 +0200

    use moment

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 const fs = require('fs')
 const express = require('express')
 const Mustache = require('mustache')
+const moment = require('moment')
 const tkoalyevents = require('tkoalyevents')
 
 const app = express()
 const port = process.env.PORT || 3000
+
+const formatDates = events =>
+  events.map(event => ({...event, starts: moment(event.starts).fromNow() }))
 
 app.use(express.static('static'))
 app.get('/', (req, res) => {
   const template = fs.readFileSync('templates/index.html', { encoding: 'utf8' })
   const view = {}
   tkoalyevents(events => {
-    const html = Mustache.render(template, {...view, events });
+    const html = Mustache.render(template, {...view, events: formatDates(events) });
     res.send(html)
   })
 })
