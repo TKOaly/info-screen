2635c411fc717af902a3c0e7f291218b68761470
Jani Luukko <jj.luukko@gmail.com>
2019-03-27 16:09:39 +0200

    asdf

---
+++ controllers/index.js
@@ -0,0 +1,65 @@
+const util = require('util')
+const React = require('react')
+const ReactDOMServer = require('react-dom/server')
+
+const App = require('../components/App')
+const tkoalyevents = require('tkoalyevents')
+
+import { SheetsRegistry } from 'jss';
+import JssProvider from 'react-jss/lib/JssProvider';
+import {
+  MuiThemeProvider,
+  createMuiTheme,
+  createGenerateClassName,
+} from '@material-ui/core/styles';
+import green from '@material-ui/core/colors/green';
+import red from '@material-ui/core/colors/red';
+
+const index = (req, res) => {
+
+  const sheetsRegistry = new SheetsRegistry();
+
+  // Create a sheetsManager instance.
+  const sheetsManager = new Map();
+
+  // Create a theme instance.
+  const theme = createMuiTheme({
+    palette: {
+      primary: green,
+      accent: red,
+      type: 'light',
+    },
+  });
+
+    // Create a new class name generator.
+    const generateClassName = createGenerateClassName();
+
+  const callback = events => {
+    const rootComponent = ReactDOMServer.renderToStaticMarkup(
+    <JssProvider registry={sheetsRegistry} generateClassName={generateClassName}>
+      <MuiThemeProvider theme={theme} sheetsManager={sheetsManager}>
+        <App events={events} />
+      </MuiThemeProvider>
+    </JssProvider>
+    )
+    // Grab the CSS from our sheetsRegistry.
+    const css = sheetsRegistry.toString()
+    const html = `
+      <html>
+        <head>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500">
+        <style id="jss-server-side">${css}</style>
+        </head>
+        <body>
+          <div id="root">
+            ${rootComponent}
+          </div>
+        </body>
+      </html>
+    `
+    res.send(html)
+  }
+  tkoalyevents(callback)
+}
+
+module.exports = index
--- index.js
+++ index.js
@@ -1,16 +1,5 @@
-const fs = require('fs')
-const express = require('express')
-const Mustache = require('mustache')
-const moment = require('moment')
-const tkoalyevents = require('tkoalyevents')
+require("@babel/register")({
+    presets: ["@babel/preset-env", "@babel/preset-react"]
+})
 
-const app = express()
-const port = process.env.PORT || 3000
-
-const formatDates = events =>
-  events.map(event => ({...event, starts: moment(event.starts).fromNow() }))
-
-app.use(express.static('static'))
-app.get('/', () => null)
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+require('./server')
