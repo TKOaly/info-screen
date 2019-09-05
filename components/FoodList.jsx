196a73ebe76a3abf5aa2d258bf093bd3b4c0738a
Jani Luukko <jani.luukko@reaktor.com>
2019-09-05 14:57:54 +0300

    Hide food list after 4pm

--- components/FoodList.jsx
+++ components/FoodList.jsx
@@ -6,18 +6,22 @@ import axios from 'axios'
 export default function FoodList(initialFoodlists) {
   const [{chemicum, exactum}, updateFoodlists] = useState(initialFoodlists)
 
+  const hour = (new Date()).getHours()
+  const isClosed = hour >= 16 // TODO: check from api response
+
   useEffect(() => {
-    const intervalId = setInterval(() =>
-      fetchFoodlists()
-        .then(newFoodlist => updateFoodlists(newFoodlist))
-        .catch(_ => {})
-    , 60000)
+    const intervalId = setInterval(() => {
+      if (!isClosed) {
+        fetchFoodlists()
+          .then(newFoodlist => updateFoodlists(newFoodlist))
+          .catch(_ => {})
+      }
+    }, 60000)
     return () => clearInterval(intervalId)
   }, [])
 
-  return (
-    <div>
-      <Carousel
+  const foodCarousel = (
+    <Carousel
         showThumbs={false}
         showArrows={false}
         infiniteLoop={true}
@@ -40,6 +44,17 @@ export default function FoodList(initialFoodlists) {
           </List>
         </div>
       </Carousel>
+  )
+
+  const gif = `<div style='position:relative; padding-bottom:calc(56.25% + 44px)'><iframe src='https://gfycat.com/ifr/AgedMiniatureBoto' frameborder='0' scrolling='no' width='100%' height='100%' style='position:absolute;top:0;left:0;' allowfullscreen></iframe></div>`
+  const foodClosed = (
+    <div dangerouslySetInnerHTML={{__html:
+     gif}}></div>
+ )
+
+  return (
+    <div>
+      {!isClosed ? foodCarousel : foodClosed}
     </div>
   )
 }
