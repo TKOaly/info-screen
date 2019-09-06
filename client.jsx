0e9ff7db99becdf347ce88c4cfb416e4bde429e1
Touko Peltomaa <touko.peltomaa@gmail.com>
2019-09-06 17:42:12 +0300

    remove client.jsx

--- client.jsx
+++
@@ -1,9 +0,0 @@
-import ReactDOM from "react-dom";
-import React from "react";
-import App from "./components/App.jsx";
-
-const rootElem = document.getElementById("root");
-
-console.log(window.INITIAL_STATE);
-
-ReactDOM.hydrate(<App initialState={window.INITIAL_STATE} />, rootElem);
