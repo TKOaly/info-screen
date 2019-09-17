5d6f90927dd0c2c4254eee5e2e9205476366abdf
Touko Peltomaa <touko.peltomaa@gmail.com>
2019-09-17 14:15:56 +0300

    use serverRuntimeConfig

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
 const withCSS = require('@zeit/next-css')
 const { parsed: localEnv } = require('dotenv').config()
-const webpack = require('webpack')
 
 module.exports = withCSS({
-  webpack(config) {
-    config.plugins.push(new webpack.EnvironmentPlugin(localEnv))
-
-    return config
-  }
+  serverRuntimeConfig: localEnv
 });
