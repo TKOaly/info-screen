774c157dede11129169fc2d3c97b7604d19940f6
Jani Luukko <jani.luukko@reaktor.com>
2020-03-17 14:54:20 +0200

    COVID19: stop fetching events if corona mode enabled

--- pages/api/events/upcoming.js
+++ pages/api/events/upcoming.js
@@ -2,8 +2,13 @@ import { fetchUpcomingEvents } from "../../../services/tkoalyEventService";
 import { fetchUpcomingEvents as fetchPajaEvents } from "../../../services/toskaPajaCalendarService"; // eslint-disable-line
 import { compareAsc } from "date-fns";
 import * as R from "ramda";
+import { enableCoronaInfo } from "../../config.json";
 
 export default function handle(req, res) {
+  if (enableCoronaInfo) {
+    res.json([]);
+    return;
+  }
   Promise.all([fetchUpcomingEvents(), fetchPajaEvents()])
     .then(([tkoalyEvents, pajaEvents]) => {
       return R.pipe(
