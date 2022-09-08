19b8baa271f8cec9f20ba5b9cab79420cbb19767
Markus Kaihola <markus.kaihola@helsinki.fi>
2022-09-08 23:35:08 +0300

    Changed unicafe api to a working one

--- services/unicafeFoodListService.js
+++ services/unicafeFoodListService.js
@@ -3,32 +3,30 @@ import { format } from "date-fns";
 import en from "date-fns/locale/en-US";
 import { groupBy } from "ramda";
 
-const EXACTUM_FOOD_URL =
-  "https://messi.hyyravintolat.fi/publicapi/restaurant/11";
-const CHEMI_FOOD_URL = "https://messi.hyyravintolat.fi/publicapi/restaurant/10";
+const BASE_URL = "https://unicafe.fi/wp-json/swiss/v1/restaurants/?lang=en";
 
 export const fetchExactumFoodlist = () => {
   return axios
-    .get(EXACTUM_FOOD_URL)
-    .then(({ data }) => data)
+    .get(BASE_URL)
+    .then(({ data }) => data[15])
     .then(formatResponse);
 };
 
 export const fetchChecmicumFoodlist = () => {
   return axios
-    .get(CHEMI_FOOD_URL)
-    .then(({ data }) => data)
+    .get(BASE_URL)
+    .then(({ data }) => data[16])
     .then(formatResponse);
 };
 
 function formatResponse(response) {
-  const { data } = response;
-  const { data: foodlistData } = data.find(
-    ({ date_en }) => date_en === format(new Date(), "EEE dd.MM", { locale: en })
+  const { menuData } = response;
+  const { data: foodlistData } = menuData.menus.find(
+    ({ date }) => date === format(new Date(), "EE dd.MM.", { locale: en })
   );
   if (foodlistData) {
-    const foodlistDataEn = foodlistData.map(({ name_en, price, meta }) => ({
-      name: name_en,
+    const foodlistDataEn = foodlistData.map(({ name, price, meta }) => ({
+      name: name,
       priceName: price.name,
       meta: { diet: meta["0"].join(" "), allergies: meta["1"].join(" ") }
     }));
