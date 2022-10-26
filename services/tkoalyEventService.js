8d77f9f844aeeea10aaeae2d364a86f00956289f
Eetu Raunio <eetu.raunio@helsinki.fi>
2022-10-27 00:39:55 +0300

    Clarify today & tomorrow, get today's events as well

--- services/tkoalyEventService.js
+++ services/tkoalyEventService.js
@@ -1,43 +1,56 @@
 import axios from "axios";
-import { isAfter, addMinutes, isSameWeek } from "date-fns";
+import {
+  isAfter,
+  addMinutes,
+  isSameWeek,
+  startOfToday,
+  formatRelative,
+  compareAsc
+} from "date-fns";
 import * as R from "ramda";
-import { compareAsc } from "date-fns";
-import { formatRelative, isSameUTCWeek } from "date-fns";
-import { fi } from "date-fns/locale";
+import { enGB } from "date-fns/locale";
 
 const ENTRYPOINT = "https://event-api.tko-aly.fi/api/events";
 
 const formatRelativeLocale = {
-  today: "'Today'",
-  tomorrow: "'Tomorrow'",
-  nextWeek: function (date, baseDate) {
+  today: "'Today', EEEE dd.MM.",
+  tomorrow: "'Tomorrow', EEEE dd.MM.",
+  nextWeek: function(date, baseDate) {
     if (isSameWeek(date, baseDate)) {
-      return "'This week'"
+      return "'This week'";
     } else {
-      return "'Next week'"
+      return "'Next week'";
     }
   },
-  other: "'Later'",
+  other: "'Later'"
 };
 
 export const customLocale = {
-  ...fi,
+  ...enGB,
   formatRelative: function(token, date, baseDate, options) {
-    let format = formatRelativeLocale[token]
-    if (typeof format === 'function') {
-      return format(date, baseDate, options)
+    let format = formatRelativeLocale[token];
+    if (typeof format === "function") {
+      return format(date, baseDate, options);
     }
-    return format
+    return format;
   }
 };
 
-const sortEvents = R.sort((a, b) => compareAsc(new Date(a.starts), new Date(b.starts)));
-const groupEvents = R.groupBy((a) => formatRelative(new Date(a.starts), new Date(), { locale: customLocale, weekStartsOn: 1 }));
+const sortEvents = R.sort((a, b) =>
+  compareAsc(new Date(a.starts), new Date(b.starts))
+);
+const groupEvents = R.groupBy(a =>
+  formatRelative(new Date(a.starts), new Date(), {
+    locale: customLocale,
+    weekStartsOn: 1
+  })
+);
 
 export const fetchUpcomingEvents = () =>
   axios
     .get(ENTRYPOINT, {
-      params: { fromDate: new Date().toJSON() }
+      // Get events that are happening today and in the future
+      params: { fromDate: startOfToday().toJSON() }
     })
     .then(res => res.data)
     .then(events =>
@@ -54,5 +67,4 @@ export const fetchUpcomingEvents = () =>
       return [];
     });
 
-export const fetchGroupedEvents = () =>
-    fetchUpcomingEvents().then(groupEvents)
+export const fetchGroupedEvents = () => fetchUpcomingEvents().then(groupEvents);
