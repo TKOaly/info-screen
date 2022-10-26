c427e0c86536dece853b4c29f4b1d015a66871d8
Eetu Raunio <eetu.raunio@helsinki.fi>
2022-10-27 00:43:00 +0300

    Properly scale event list subtitles

--- components/EventList.jsx
+++ components/EventList.jsx
@@ -4,8 +4,13 @@ import useSWR from "swr";
 import { red } from "@mui/material/colors";
 import Event from "./Event/Event";
 
-const smallHeaderStyle = { fontSize: "18px", fontWeight: "700" };
-const errorHeaderStyle = {
+const subtitleHeadingStyle = {
+  fontSize: "1.5rem",
+  fontWeight: "700"
+};
+const errorHeadingStyle = {
+  fontSize: "18px",
+  fontWeight: "700",
   opacity: 0.5,
   color: red[500],
   marginBottom: "1rem"
@@ -21,17 +26,14 @@ const EventList = () => {
   return (
     <List dense={true}>
       {error && (
-        <Typography
-          variant="h3"
-          sx={{ ...smallHeaderStyle, ...errorHeaderStyle }}
-        >
+        <Typography variant="h3" sx={{ ...errorHeadingStyle }}>
           Failed to load events, this list may be out of date.
         </Typography>
       )}
       {events &&
         Object.entries(events).map(([subtitle, events]) => (
           <React.Fragment key={subtitle}>
-            <Typography variant="h3" sx={smallHeaderStyle}>
+            <Typography variant="h3" sx={subtitleHeadingStyle}>
               {subtitle}
             </Typography>
             {events.map(event => (
